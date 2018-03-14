db7626d40ae7c8b74bf66f4711e93c03f795aabf
zhuchaoyue <zhuchaoyue@lehoolive.com>
2018-03-14 18:18:49 +0800

    update

--- orm.py
+++ orm.py
@@ -95,3 +95,8 @@ class Field(object):
 
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
