dd353f371984766262a5e3bd5864884aeb2fab7a
zhuchaoyue <zhuchaoyue@lehoolive.com>
2018-03-14 17:26:46 +0800

    update

---
+++ orm.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : Mar 14,2018
+# File    : orm.py.py
+# Author  : Hyzen
+import asyncio, logging
+import aiomysql
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['root'],
+        password=kw['137854'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf-8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            afftected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return afftected
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
